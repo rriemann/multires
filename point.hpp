4431bd64c92856a8c913aac808eb59f3c88c7e40
Robert Riemann <robert@riemann.cc>
2014-03-12 16:16:41 +0100

    docu point_t

--- point.hpp
+++ point.hpp
@@ -1,5 +1,5 @@
 /***************************************************************************************
- * Copyright (c) 2013 Robert Riemann <robert@riemann.cc>                                *
+ * Copyright (c) 2014 Robert Riemann <robert@riemann.cc>                                *
  *                                                                                      *
  * This program is free software; you can redistribute it and/or modify it under        *
  * the terms of the GNU General Public License as published by the Free Software        *
@@ -17,15 +17,25 @@
 #ifndef POINT_HPP
 #define POINT_HPP
 
-#include <strings.h> // ffs
+#include <strings.h> // provides the function ffs()
 
 #include "settings.h"
 
+/*!
+   \brief The point_t class represents one point in the grid—independently of the grid type.
+ */
 class point_t
 {
 public:
-    point_t() {}
+    point_t() {} //!< empty constructor provided for default-constructing elements in a std container type
 
+    /*!
+       \brief constructor that initalizes \ref m_index and the physical position in space \ref m_x
+       \param index of this node evaluated in the level level_max
+       \param level_max
+
+       \sa g_x0, g_span in settings.h
+     */
     point_t(index_t index, const u_char level_max) :
         m_index(index)
     {
@@ -34,12 +44,28 @@ public:
         }
     }
 
+    /*!
+       \brief constructor that initializes \ref m_index, he physical position in space \ref m_x and a field value \ref m_phi
+       \param index of this node evaluated in the level level_max
+       \param level_max
+       \param f_eval field generator which is used to initalize \ref m_phi
+
+       \sa point_t(index_t, const u_char)
+     */
     point_t(index_t index, u_char level_max, field_generator_t f_eval) :
         point_t(index, level_max)
     {
         m_phi = f_eval(m_x);
     }
 
+    /*!
+       \brief constructor that initializes \ref m_index, he physical position in space \ref m_x and a field value \ref m_phi
+       \param index of this node evaluated in the level level_max
+       \param level_max
+       \param value to initialize \ref m_phi
+
+       \sa point_t(index_t, const u_char)
+     */
     point_t(index_t index, u_char level_max, real phi) :
         point_t(index, level_max)
     {
@@ -55,6 +81,13 @@ public:
     void setIndex(index_t index)
     { m_index = index; }
 
+    /*!
+       \brief determines the first level where this point might be part of
+       \param level_max is the maximum level on which indexing is based on
+       \return the level of this node
+
+       This function is mostly provided for debugging purposes.
+     */
     int getLevel(const u_char level_max) const
     {
         int min = (1 << level_max);
@@ -69,11 +102,10 @@ public:
         }
     }
 
-    /*! the index with respect to level_max */
-    index_t m_index;
-    location_t m_x;
-    real m_flow;
-    real m_phi;
-    point_t *m_next;
+    index_t m_index; //!< index with respect to level_max in \ref point_t()
+    location_t m_x; //!< point location in physical space
+    real m_flow; //!< takes the flow calculated by \ref flowHelper()
+    real m_phi; //!< actual field variable
+    point_t *m_next; //!< part of the forward-only linked list throughout all points in the grid
 };
 #endif // POINT_HPP
