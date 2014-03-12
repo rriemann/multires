8a8e27163b62b3349b2b7de4da7c54e2aa6a00e3
Robert Riemann <robert@riemann.cc>
2014-03-12 15:49:12 +0100

    document the grid class

--- grid.hpp
+++ grid.hpp
@@ -23,14 +23,34 @@ class grid_t
 {
 public:
     grid_t();
+
+    /*!
+       \brief getTime returns the current global time of this grid
+       \return returns the current global time of his grid
+     */
     real getTime()
     { return m_time; }
 
+    /*!
+       \brief timeStep evolves the grid to the next point in time
+
+       The size of the time step might depend on the current configuration of
+       the grid. The choosen value is given as a return value.
+
+       \return the size of the time step
+
+       \sa getTime()
+     */
     virtual real timeStep() = 0;
+
+    /*!
+       \brief size gives back the number of points of type point_t in this grid
+       \return the number of points in this grid
+     */
     virtual size_t size() const = 0;
 
 protected:
-    real m_time = 0;
+    real m_time = 0; ///< global time
 };
 
 #endif // GRID_HPP
